45228e86dfa7b68447a486413a1482b612417aa4
ekki6 <robbiejupiter@proton.me>
2026-07-01 16:39:53 -0700

    broke out dbconfig JSON

--- pgconnect.py
+++ pgconnect.py
@@ -1,11 +1,21 @@
+#simple script to check postgres connection
+import json
 import psycopg
 
-conn = psycopg.connect(
-    dbname="analysis",          # or whatever database you're using
-    user="postgres",
-    password="Duluth/77",
-    host="localhost",
-)
+#replaced below with JSON bit
+#conn = psycopg.connect(
+#    dbname="analysis",
+#    user="postgres",
+#    password="Duluth/77",
+#    host="localhost",
+#)
+
+with open("dbconfig.json") as f:
+    config = json.load(f)
+
+
+conn = psycopg.connect(**config)
+
 
 with conn.cursor() as cur:
     cur.execute("""
