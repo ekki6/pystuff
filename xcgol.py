2ab86a5f562c4c44857aa54618815383d3c3e725
ekki6 <robbiejupiter@proton.me>
2026-07-26 10:13:33 +1000

    finished cgol

--- xcgol.py
+++ xcgol.py
@@ -1,36 +1,66 @@
 SIZE = 20
 
+#grid = [
+#    ["1" if (r + c) % 2 == 0 else "0" for c in range(SIZE)]
+#    for r in range(SIZE)
+#]
+
 grid = [
-    ["1" if (r + c) % 2 == 0 else "0" for c in range(SIZE)]
-    for r in range(SIZE)
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,1,0,0,0,0],
+    [0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,1,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,1,1,0,0,0,1,0,1,0,0,0],
+    [0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0,1,0,0,0],
+    [0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,0,0,0,0],
+    [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 ]
+
 
 def local_sum(r,c):
     acc = 0
     if r > 0 and c > 0:
-        acc += int(grid[r-1][c-1])
+        acc += grid[r-1][c-1]
     if r > 0:
-        acc += int(grid[r-1][c])
+        acc += grid[r-1][c]
     if r > 0 and c < (SIZE - 1):
-        acc += int(grid[r-1][c+1])
+        acc += grid[r-1][c+1]
     if c > 0:
-        acc += int(grid[r][c-1])
+        acc += grid[r][c-1]
     if c < (SIZE - 1):
-        acc += int(grid[r][c+1])
-    if r < 0 and c < (SIZE - 1) :
-        acc += int(grid[r+1][c+1])
-    if c < (SIZE - 1):
-        acc += int(grid[r][c+1])
+        acc += grid[r][c+1]
+    if r < (SIZE - 1) and c > 0:
+        acc += grid[r+1][c-1]
+    if r < (SIZE - 1):
+        acc += grid[r+1][c]
     if r < (SIZE - 1) and c < (SIZE - 1):
-        acc += int(grid[r+1][c+1])
+        acc += grid[r+1][c+1]
     return acc
 
 while True:
 
     for row in grid:
-        print(" ".join(row))
-
+        print(" ".join(map(str, row)))
+    
     key = input("\n<Enter> for next generation, q to quit: ")
+
+    grid2 = [
+        [0 for c in range(SIZE)]
+        for r in range(SIZE)
+    ]
 
     if key.lower() == "q":
         break
@@ -38,16 +68,18 @@ while True:
     # find births, deaths (and survives)
     for r in range(SIZE):
         for c in range(SIZE):
-            if grid[r][c] == "1":
-                if (local_sum(r,c) == 2 or local_sum(r,c) == 3):
-                    grid[r][c] = "1"
+            lsum = local_sum(r,c)
+            if grid[r][c] == 1:
+                if ((lsum == 2) or (lsum == 3)):
+                    grid2[r][c] = 1
                 else:
-                    grid[r][c] = "0"
-            if grid[r][c] == "0":
-                if local_sum(r,c) == 3:
-                    grid[r][c] = "1"
+                    grid2[r][c] = 0
+            if grid[r][c] == 0:
+                if lsum == 3:
+                    grid2[r][c] = 1
                 else:
-                    grid[r][c] = "0"
+                    grid2[r][c] = 0
+    grid = grid2
                     
 
     print()      # blank line between generations
